cff3d6eb8a061e319fe44ffb16d7110065380544
Einar Uvsløkk <einar.uvslokk@gmail.com>
2013-09-19 12:23:12 +0200

    add test to load swipejs

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -30,21 +30,16 @@
 	function initImgGallery() {
 
 		var nav = $('#slider').find('nav'),
-			wrapper = $('#wrapper');
+			wrapper = $('#wrapper'),
+			swipeEnabled = false;
 
 		yepnope({
-			load: 'http://cdnjs.cloudflare.com/ajax/libs/swipe/2.0/swipe.min.js',
-			complete: function() {
+			test: window.Swipe,
+			nope: 'http://cdnjs.cloudflare.com/ajax/libs/swipe/2.0/swipe.min.js',
+			callback: function() {
 				if (window.Swipe) {
-					window.slider = new Swipe(document.getElementById('slider'), {
-						callback: function(index, element) {
-							updateNav(index);
-							loadImg(index + 1);
-						}
-					});
-					wrapper.on('touchstart', function(e) {
-						loadImg(window.slider.getPos() + 1);
-					});
+					swipeEnabled = true;
+					initSwipe();
 				}
 			}
 		});
@@ -56,15 +51,33 @@
 
 			loadImg(index);
 
-			slider.slide(index);
+			if (swipeEnabled) {
+				slider.slide(index);
+			}
 
 			updateNav(index);
 		});
+
+		function initSwipe() {
+			window.slider = new Swipe(document.getElementById('slider'), {
+				callback: function(index, element) {
+					updateNav(index);
+					loadImg(index + 1);
+				}
+			});
+			wrapper.on('touchstart', function(e) {
+				loadImg(window.slider.getPos() + 1);
+			});
+		}
 
 		function loadImg(index) {
 			var imgs = $('#wrapper').find('img'),
 				img = imgs.eq(index),
 				imgSrc = img.data('imgsrc');
+
+			if (!swipeEnabled) {
+				// TODO make gallery work without swipe enabled also
+			}
 
 			if (img.attr('src') !== imgSrc) {
 				img.on('load', function() {
